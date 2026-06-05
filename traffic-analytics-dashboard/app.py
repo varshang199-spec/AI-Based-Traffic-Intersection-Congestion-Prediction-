39298b4daa81683f86465490488718485d343e0a
varshang199-spec <varshang199@gmail.com>
2026-06-05 20:17:33 +0530

    Create app.py

---
+++ traffic-analytics-dashboard/app.py
@@ -0,0 +1,28 @@
+traffic-analytics-dashboard/
+│
+├── app.py
+│
+├── data/
+│   ├── Traffic.csv
+│   └── TrafficTwoMonth.csv
+│
+├── pages/
+│   ├── 1_Executive_Dashboard.py
+│   ├── 2_Traffic_Patterns.py
+│   ├── 3_Vehicle_Analysis.py
+│   ├── 4_Congestion_Analysis.py
+│   ├── 5_Forecasting.py
+│
+├── utils/
+│   ├── data_loader.py
+│   ├── charts.py
+│   ├── insights.py
+│
+├── assets/
+│   ├── logo.png
+│   └── banner.png
+│
+├── requirements.txt
+├── README.md
+└── .streamlit/
+    └── config.toml

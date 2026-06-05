fe4b720d79403ea02d634e472d331835b92d7e1a
varshang199-spec <varshang199@gmail.com>
2026-06-05 21:28:27 +0530

    Update 1_Executive_Dashboard.py

--- pages/1_Executive_Dashboard.py
+++ pages/1_Executive_Dashboard.py
@@ -1,7 +1,21 @@
-from utils.data_loader import load_data
-from utils.charts import create_kpi_cards
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
-def main():
-    df = load_data()
+numeric_cols = traffic_two_month.select_dtypes(include="number").columns
 
-    create_kpi_cards(df)
+target = st.selectbox(
+    "Select Forecast Column",
+    numeric_cols
+)
+
+data = traffic_two_month[target].dropna()
+
+X = np.arange(len(data)).reshape(-1,1)
+y = data.values
+
+model = LinearRegression()
+model.fit(X,y)
+
+future_x = np.arange(len(data)+30).reshape(-1,1)
+
+predictions = model.predict(future_x)

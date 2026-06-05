dbbf1656bcc1b93701dd5af1215ebe2995f62bc4
varshang199-spec <varshang199@gmail.com>
2026-06-05 21:34:50 +0530

    Update 5_Forecasting.py

--- pages/5_Forecasting.py
+++ pages/5_Forecasting.py
@@ -1,7 +1,45 @@
-from utils.data_loader import load_two_month_data
-from utils.charts import forecast_chart
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import plotly.graph_objects as go
 
-def main():
-    df = load_two_month_data()
+from analysis import load_data
 
-    forecast_chart(df)
+traffic, traffic2 = load_data()
+
+st.title("🔮 Traffic Forecasting")
+
+numeric_cols = traffic.select_dtypes(include='number')
+
+column = st.selectbox(
+    "Select Column",
+    numeric_cols.columns
+)
+
+data = traffic[column].dropna()
+
+X = [[i] for i in range(len(data))]
+y = data.values
+
+model = LinearRegression()
+model.fit(X, y)
+
+future = [[i] for i in range(len(data)+20)]
+
+pred = model.predict(future)
+
+fig = go.Figure()
+
+fig.add_scatter(
+    y=y,
+    mode="lines",
+    name="Actual"
+)
+
+fig.add_scatter(
+    y=pred,
+    mode="lines",
+    name="Forecast"
+)
+
+st.plotly_chart(fig, use_container_width=True)

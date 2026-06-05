3f76e19002cdb2e8d36ecabb9c51c21e54a4d337
varshang199-spec <varshang199@gmail.com>
2026-06-05 21:16:55 +0530

    Update analysis.py

--- notebook/analysis.py
+++ notebook/analysis.py
@@ -1,55 +1,8 @@
 import pandas as pd
 
-def load_data(file_path):
-    df = pd.read_csv(file_path)
+def load_data():
 
-    df["DateTime"] = pd.to_datetime(df["DateTime"])
+    traffic = pd.read_csv("data/Traffic.csv")
+    traffic2 = pd.read_csv("data/TrafficTwoMonth.csv")
 
-    df["Year"] = df["DateTime"].dt.year
-    df["Month"] = df["DateTime"].dt.month
-    df["Day"] = df["DateTime"].dt.day
-    df["Hour"] = df["DateTime"].dt.hour
-    df["Weekday"] = df["DateTime"].dt.day_name()
-
-    return df
-
-
-def get_kpis(df):
-    return {
-        "Total Vehicles": int(df["Vehicles"].sum()),
-        "Average Traffic": round(df["Vehicles"].mean(), 2),
-        "Maximum Traffic": int(df["Vehicles"].max()),
-        "Total Junctions": df["Junction"].nunique()
-    }
-
-
-def hourly_traffic(df):
-    return (
-        df.groupby("Hour")["Vehicles"]
-        .mean()
-        .reset_index()
-    )
-
-
-def junction_traffic(df):
-    return (
-        df.groupby("Junction")["Vehicles"]
-        .sum()
-        .reset_index()
-    )
-
-
-def monthly_traffic(df):
-    return (
-        df.groupby("Month")["Vehicles"]
-        .sum()
-        .reset_index()
-    )
-
-
-def weekday_traffic(df):
-    return (
-        df.groupby("Weekday")["Vehicles"]
-        .mean()
-        .reset_index()
-    )
+    return traffic, traffic2
